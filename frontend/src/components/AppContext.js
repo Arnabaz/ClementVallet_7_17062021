fb6c4de95969b201bb94575a3970848918e48b13
Clement Vallet <clement.vallet@gmail.com>
2022-02-11 20:26:11 +0100

    feat: add AppContext component

---
+++ frontend/src/components/AppContext.js
@@ -0,0 +1,8 @@
+// --- IMPORTS ---
+import {createContext} from "react";
+
+// Pour stocker si l'utilisateur est authentifié ou non
+const AuthContext = createContext({});
+
+// --- EXPORT ---
+export {AuthContext}
