8a048167785104b9120627a54cf59d90f92895ee
Clement Vallet <clement.vallet@gmail.com>
2022-02-11 19:15:40 +0100

    feat: add CORS

--- backend/app.js
+++ backend/app.js
@@ -6,5 +6,13 @@ const db = require("./config/db.config");
 // --- APP ---
 const app = express();
 
+// Cors
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
 // --- EXPORT ---
 module.exports = app;
