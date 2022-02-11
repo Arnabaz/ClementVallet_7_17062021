5e5a69de8c169ce3eee2c9645f6788da8992458c
Clement Vallet <clement.vallet@gmail.com>
2022-02-11 20:01:03 +0100

    feat: organizing src directory in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+// --- IMPORTS ---
+import React, {useState, useEffect} from "react";
+import {AuthContext} from "./components/AppContext";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+// Import des composants
+import Home from "./pages/Home";
+import Feed from "./pages/Feed";
+import Profile from "./pages/Profile";
+import Admin from "./pages/Admin";
+import RequireAuth from "./components/RequireAuth"
 
-function App() {
+// --- APP ---
+const App = () => {
+// Déclaration de variables :
+  let token = localStorage.getItem("token");
+
+  // Déclaration de states :
+  const [authContext, setAuthContext] = useState(false);
+
+  useEffect(() => {
+    if (token) {
+      setAuthContext(true);
+    } else {
+      setAuthContext(false)
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <AuthContext.Provider value={{authContext, setAuthContext}}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route element={<RequireAuth/>}>
+              <Route path="/feed" element={<Feed/>}/>
+              <Route path="/profile" element={<Profile/>}>
+                <Route path="/profile/:userId" element={<Profile/>}/>
+              </Route>
+              <Route path="/admin" element={<Admin/>}/>
+            </Route>
+          </Routes>
+        </Router>
+      </AuthContext.Provider>
+  )
 }
 
+
+// --- EXPORT ---
 export default App;
